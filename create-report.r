1f26f9797ce88a1ec1a76f774c8333811d445889
warren-davies4 <112185584+warren-davies4@users.noreply.github.com>
2024-04-16 13:39:26 +0000

    added create-report.r

--- create-report.r
+++ create-report.r
@@ -1,27 +1,27 @@
 library(dplyr)
 
-if (!file.exists(destination)) {
-    download.file(
-        "https://s3.eu-west-2.amazonaws.com/files.digital.nhs.uk/assets/Services/Artificial+data/Artificial+HES+final/", 
-        "./"
-    )
-} else {
-    print( "file already exists" )
-}
+download.file(
+  "https://s3.eu-west-2.amazonaws.com/files.digital.nhs.uk/assets/Services/Artificial+data/Artificial+HES+final/artificial_hes_ae_202302_v1_sample.zip", 
+  "./artificial_hes_ae_202302_v1_sample.zip"
+)
+
 
 unzip(
-    "./artificial_hes_ae_202302_v1_sample.zip"
+  "./artificial_hes_ae_202302_v1_sample.zip"
 )
 
 df_hes <- read.csv(
-    "./artificial_hes_ae_202302_v1_sample.zip"
+  "./artificial_hes_ae_202302_v1_sample/artificial_hes_ae_2122.csv"
 )
 
-df_hes_england_episode_count <- df_hes %>% summarise({{"EPIKEY"}} := n_distinct(df_input[["EPIKEY"]]))
+
+df_hes_england_episode_count <- df_hes %>% summarise(
+  "EPIKEY" := n_distinct(df_hes[["EPIKEY"]])
+)
 
 write.csv(
-    df_hes_england_episode_count,
-    file = "./hes_england_episode_count.csv", sep = ""),
-    row.names = FALSE,
-    quote = FALSE
+  df_hes_england_episode_count,
+  file = "./hes_england_episode_count.csv",
+  row.names = FALSE,
+  quote = FALSE
 )
