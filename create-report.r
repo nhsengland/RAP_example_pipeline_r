68245151c988d33b7403a4ef71587fbf4f2d7304
warren-davies4 <112185584+warren-davies4@users.noreply.github.com>
2024-04-29 16:36:39 +0100

    moved download and aggregation code into functions (#3)

--- create-report.r
+++ create-report.r
@@ -1,10 +1,32 @@
 library(dplyr)
 
-download.file(
+get_distinct_count_of_col <- function(df_input, col_name) {
+
+  df_output <- df_hes %>% summarise(
+    {{col_name}} := n_distinct(df_input[[col_name]])
+  )
+
+  return(df_output)
+
+}
+
+download_file_if_not_exists <- function(url, destination) {
+
+  if (!file.exists(destination)) {
+    download.file(
+      url, 
+      destination
+    )    
+  } else {
+    print("File already exists.")
+  }
+
+}
+
+download_file_if_not_exists(
   "https://s3.eu-west-2.amazonaws.com/files.digital.nhs.uk/assets/Services/Artificial+data/Artificial+HES+final/artificial_hes_ae_202302_v1_sample.zip", 
   "./artificial_hes_ae_202302_v1_sample.zip"
 )
-
 
 unzip(
   "./artificial_hes_ae_202302_v1_sample.zip"
@@ -14,10 +36,7 @@ df_hes <- read.csv(
   "./artificial_hes_ae_202302_v1_sample/artificial_hes_ae_2122.csv"
 )
 
-
-df_hes_england_episode_count <- df_hes %>% summarise(
-  "EPIKEY" := n_distinct(df_hes[["EPIKEY"]])
-)
+df_hes_england_episode_count = get_distinct_count_of_col(df_hes, "EPIKEY")
 
 write.csv(
   df_hes_england_episode_count,
