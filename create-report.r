61959b56dcd7f0860ce29fd138b2c22561c22013
warren-davies4 <112185584+warren-davies4@users.noreply.github.com>
2024-05-17 12:33:20 +0000

    updated files paths in create_report.r to match new structure

--- create-report.r
+++ create-report.r
@@ -6,22 +6,23 @@ source("src/data_processing/aggregations.r")
 
 download_file_if_not_exists(
   "https://files.digital.nhs.uk/assets/Services/Artificial%20data/Artificial%20HES%20final/artificial_hes_ae_202302_v1_sample.zip", 
-  "./artificial_hes_ae_202302_v1_sample.zip"
+  "./data_in/artificial_hes_ae_202302_v1_sample.zip"
 )
 
 unzip(
-  "./artificial_hes_ae_202302_v1_sample.zip"
+  "./data_in/artificial_hes_ae_202302_v1_sample.zip",
+  exdir="./data_in/"
 )
 
 df_hes <- read.csv(
-  "./artificial_hes_ae_202302_v1_sample/artificial_hes_ae_2122.csv"
+  "./data_in/artificial_hes_ae_202302_v1_sample/artificial_hes_ae_2122.csv"
 )
 
 df_hes_england_episode_count = get_distinct_count_of_col(df_hes, "EPIKEY")
 
 write.csv(
   df_hes_england_episode_count,
-  file = "./hes_england_episode_count.csv",
+  file = "./data_out/hes_england_episode_count.csv",
   row.names = FALSE,
   quote = FALSE
 )
