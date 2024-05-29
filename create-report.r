8ba16669fba477f3f5f3293249026810c0eb448a
warren-davies4 <112185584+warren-davies4@users.noreply.github.com>
2024-05-29 11:41:49 +0000

    imported config package into create-report.r

--- create-report.r
+++ create-report.r
@@ -1,5 +1,6 @@
 library(dplyr)
 library(docstring)
+library(config)
 
 source("src/data_ingestion/import_data.r")
 source("src/data_processing/aggregations.r")
@@ -13,11 +14,11 @@ download_file_if_not_exists(
 
 unzip(
   paste(config$download_destination, config$hes_data_file_name, sep=""),
-  config$download_destination
+  exdir = config$download_destination
 )
 
 df_hes <- read.csv(
-  paste(config$download_destination, config$downloaded_hes_folder, config$downloaded_hes_file, sep="")
+  paste(config$downloaded_hes_folder, config$downloaded_hes_file, sep="")
 )
 
 df_hes_england_episode_count = get_distinct_count_of_col(df_hes, "EPIKEY")
