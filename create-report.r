6465bfcb5b6de89a605c0d121dc9000a09972f89
warren-davies4 <112185584+warren-davies4@users.noreply.github.com>
2024-05-29 12:18:11 +0000

    changed paste to paste0

--- create-report.r
+++ create-report.r
@@ -8,17 +8,17 @@ source("src/data_processing/aggregations.r")
 config <- config::get()
 
 download_file_if_not_exists(
-  paste(config$hes_data_url, config$hes_data_file_name, sep=""), 
-  paste(config$download_destination, config$hes_data_file_name, sep="")
+  paste0(config$hes_data_url, config$hes_data_file_name), 
+  paste0(config$download_destination, config$hes_data_file_name)
 )
 
 unzip(
-  paste(config$download_destination, config$hes_data_file_name, sep=""),
+  paste0(config$download_destination, config$hes_data_file_name),
   exdir = config$download_destination
 )
 
 df_hes <- read.csv(
-  paste(config$downloaded_hes_folder, config$downloaded_hes_file, sep="")
+  paste0(config$downloaded_hes_folder, config$downloaded_hes_file)
 )
 
 df_hes_england_episode_count = get_distinct_count_of_col(df_hes, "EPIKEY")
