8708696c842a49ec67fb0da9afd038f6990139a3
warren-davies4 <112185584+warren-davies4@users.noreply.github.com>
2024-05-29 11:38:51 +0000

    added config file and parameterised create-report.r

--- create-report.r
+++ create-report.r
@@ -4,25 +4,27 @@ library(docstring)
 source("src/data_ingestion/import_data.r")
 source("src/data_processing/aggregations.r")
 
+config <- config::get()
+
 download_file_if_not_exists(
-  "https://files.digital.nhs.uk/assets/Services/Artificial%20data/Artificial%20HES%20final/artificial_hes_ae_202302_v1_sample.zip", 
-  "./data_in/artificial_hes_ae_202302_v1_sample.zip"
+  paste(config$hes_data_url, config$hes_data_file_name, sep=""), 
+  paste(config$download_destination, config$hes_data_file_name, sep="")
 )
 
 unzip(
-  "./data_in/artificial_hes_ae_202302_v1_sample.zip",
-  exdir="./data_in/"
+  paste(config$download_destination, config$hes_data_file_name, sep=""),
+  config$download_destination
 )
 
 df_hes <- read.csv(
-  "./data_in/artificial_hes_ae_202302_v1_sample/artificial_hes_ae_2122.csv"
+  paste(config$download_destination, config$downloaded_hes_folder, config$downloaded_hes_file, sep="")
 )
 
 df_hes_england_episode_count = get_distinct_count_of_col(df_hes, "EPIKEY")
 
 write.csv(
   df_hes_england_episode_count,
-  file = "./data_out/hes_england_episode_count.csv",
+  file = paste(config$output_path, "hes_england_episode_count.csv"),
   row.names = FALSE,
   quote = FALSE
 )
