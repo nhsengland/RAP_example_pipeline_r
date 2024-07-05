832627801a1f14d8a8892fa01454741b6dd3a0cc
warren-davies4 <112185584+warren-davies4@users.noreply.github.com>
2024-07-05 13:07:42 +0000

    added AAA comments to test for get_distinct_count_of_col()

--- tests/testthat/test_aggregations.r
+++ tests/testthat/test_aggregations.r
@@ -6,6 +6,7 @@ source("src/data_processing/aggregations.r")
 
 test_that("get_distinct_count_of_col works", {
     
+    # Arrange
     df_test_data <- data.frame(
         test_column = c("A", "B", "C", "D", "D", NULL)
     )
@@ -14,7 +15,9 @@ test_that("get_distinct_count_of_col works", {
         test_column = c(4)
     )
   
+    # Act
     df_actual = get_distinct_count_of_col(df_test_data, "test_column")
 
+    # Assert
     expect_equal(df_actual, df_expected)
 })
